b2a817aab7e48621f942d0c522b624f96ed4f1ea
lorenzo <lorenzotijerina@gmail.com>
2023-02-05 12:07:30 -0500

    error Uncaught (in promise) TypeError: Cannot read properties of null (reading 'useMemo')

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,9 +38,6 @@ class ApplicationController < ActionController::API
     end
 
 
-
-
-
     private
     def snake_case_params
         params.deep_transform_keys!(&:underscore)
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import configureStore from "./store/index";
+import csrfFetch, { restoreSession } from "./store/csrf";
+import * as sessionActions from "./store/session";
+import { createUser, loginUser, logoutUser } from "./store/usersReducer";
 
-ReactDOM.render(
-  <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const store = configureStore();
+
+const initializeApp = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+restoreSession().then(initializeApp);
+
+if (process.env.NODE_ENV !== "production") {
+  window.createUser = createUser;
+  window.loginUser = loginUser;
+  window.logoutUser = logoutUser;
+  window.store = store;
+  window.csrfFetch = csrfFetch;
+  window.sessionActions = sessionActions;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+const renderApplication = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+};
+
+if (
+  sessionStorage.getItem("currentUser") === null ||
+  sessionStorage.getItem("X-CSRF-Token") === null
+) {
+  store.dispatch(sessionActions.restoreSession()).then(renderApplication);
+} else {
+  renderApplication();
+}
