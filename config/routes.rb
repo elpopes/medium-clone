abb7d50950a86051849eb92cb3117f3f5d2ddadb
lorenzo <lorenzotijerina@gmail.com>
2023-02-06 15:42:30 -0500

    set up render build shell scripts

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
       resources :users, only: [:create, :show, :update, :destroy]
       resource :session, only: [:create, :show, :destroy]
     end
+
+    get '*path', to: "static_pages#frontend_index"
   end
  
