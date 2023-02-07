2f0bb81542b5e4c5f396bf91ccfc9d4c10b3c151
lorenzo <lorenzotijerina@gmail.com>
2023-02-07 11:10:08 -0500

    hero banner start complete:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 import SignedOutNav from "./components/signedOutNav/index";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Modal } from "./context/Modal";
 import LoginForm from "./components/LoginFormModel/LoginForm";
+import HeroBanner from "./components/HeroBanner";
 
 function App() {
   return (
     <>
       <Router>
         <SignedOutNav />
-        <LoginForm>
-          <h1>Hello from Medium-Earth</h1>
-        </LoginForm>
+        <HeroBanner />
+        <Modal>
+          <LoginForm>
+            <h1>Hello from Medium-Earth</h1>
+          </LoginForm>
+        </Modal>
       </Router>
     </>
   );
---
+++ frontend/src/components/HeroBanner/index.js
@@ -0,0 +1,18 @@
+import "./HeroBanner.css"
+
+const HeroBanner = () => {
+    return (
+        <div className="hero-banner">
+            <div className="text" >
+            <h1>Fly, you fools!</h1>
+            <h3>"There is nothing like looking if you want to find something."</h3>
+            <button className="sign-in-button">Adventure here</button>
+            </div>
+            <div className="animation">
+
+            </div>
+        </div>
+    )
+}
+
+export default HeroBanner
