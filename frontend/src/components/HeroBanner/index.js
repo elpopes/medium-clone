69ac676a35d28054d2a59303fdd1618dfd12f1b3
lorenzo <lorenzotijerina@gmail.com>
2023-02-08 17:26:18 -0500

    Hero Banner Sign Up complete

--- frontend/src/components/HeroBanner/index.js
+++ frontend/src/components/HeroBanner/index.js
@@ -1,18 +1,17 @@
-import "./HeroBanner.css"
+import "./HeroBanner.css";
+import BannerButton from "../BannerButton";
 
 const HeroBanner = () => {
-    return (
-        <div className="hero-banner">
-            <div className="text" >
-            <h1>Fly, you fools!</h1>
-            <h3>"There is nothing like looking if you want to find something."</h3>
-            <button className="sign-in-button">Adventure here</button>
-            </div>
-            <div className="animation">
+  return (
+    <div className="hero-banner">
+      <div className="text">
+        <h1>Fly, you fools!</h1>
+        <h3>"There is nothing like looking if you want to find something."</h3>
+        <BannerButton></BannerButton>
+      </div>
+      <div className="animation"></div>
+    </div>
+  );
+};
 
-            </div>
-        </div>
-    )
-}
-
-export default HeroBanner
+export default HeroBanner;
