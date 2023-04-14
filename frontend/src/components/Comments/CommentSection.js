fd7e7699220c18ca707571001faf12f99440cfbd
lorenzo <lorenzotijerina@gmail.com>
2023-04-14 10:32:00 -0400

    complete counter logic for retorts

--- frontend/src/components/Comments/CommentSection.js
+++ frontend/src/components/Comments/CommentSection.js
@@ -1,18 +1,23 @@
 import "./CommentSection.css";
 
 const CommentSection = ({ storyId, showComments, toggleComments }) => {
+  const counter = 0;
+
   return (
     <div className={`comments-section ${showComments ? "show" : ""}`}>
+      <span className="cancel-button" onClick={toggleComments}>
+        &times;
+      </span>
+      <span className="retorts-title">
+        {counter > 0 ? `Retorts (${counter})` : "Retorts"}
+      </span>
       {showComments && (
         <>
-          <div className="cancel-button" onClick={toggleComments}>
-            &times;
+          <div className="post-comment">
+            {/* PostComment component for the given storyId will be rendered here */}
           </div>
           <div className="comments-list">
             {/* Comments for the given storyId will be displayed here */}
-          </div>
-          <div className="post-comment">
-            {/* PostComment component for the given storyId will be rendered here */}
           </div>
         </>
       )}
