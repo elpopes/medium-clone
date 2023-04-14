49c6fb00c5439c7992d0b3366b45957ef2051555
lorenzo <lorenzotijerina@gmail.com>
2023-04-14 17:13:50 -0400

    add byline

--- frontend/src/components/Comments/CommentPost.js
+++ frontend/src/components/Comments/CommentPost.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import ByLine from "../Avatar/byLine";
+import { createComment } from "../../store/commentsReducer";
+import { updateComment } from "../../store/commentsReducer";
 
-const CommentPost = ({
-  storyId,
-  parentId,
-  comment,
-  createComment,
-  updateComment,
-  authorId,
-}) => {
+const CommentPost = ({ storyId, parentId, comment }) => {
+  const authorId = useSelector((state) => state.session.user?.id);
   const [body, setBody] = useState(comment ? comment.body : "");
 
   const handleSubmit = (e) => {
@@ -28,18 +26,21 @@ const CommentPost = ({
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="body">Body</label>
-        <textarea
-          name="body"
-          id="body"
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <div>
+      {authorId && <ByLine userId={authorId} />}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="body">Body</label>
+          <textarea
+            name="body"
+            id="body"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 };
 
