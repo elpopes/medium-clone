d535b539a762c19b2b202a671f65650f6f78619a
lorenzo <lorenzotijerina@gmail.com>
2023-04-26 17:43:36 -0400

    animation

--- frontend/src/components/HeroBanner/animees.js
+++ frontend/src/components/HeroBanner/animees.js
@@ -1,7 +1,43 @@
-export default function AnimateMes() {
-  const mes = document.querySelectorAll(".me");
-  for (let i = 0; i < mes.length; i++) {
-    const delay = Math.floor(Math.random() * 2000);
-    mes[i].style.animationDelay = delay + "ms";
-  }
+import React, { useEffect, useRef } from "react";
+
+export default function AnimateMes({ height, windowWidth }) {
+  const animationDiv = useRef(null);
+
+  useEffect(() => {
+    const numMes = 20; // Adjust this value to control the number of "ME"s
+    const minWidth = 650;
+
+    const resizeAnimationContainer = () => {
+      if (windowWidth >= minWidth) {
+        animationDiv.current.style.height = `${height}px`;
+      } else {
+        animationDiv.current.style.height = "0";
+      }
+    };
+
+    resizeAnimationContainer();
+
+    if (windowWidth >= minWidth) {
+      for (let i = 0; i < numMes; i++) {
+        const me = document.createElement("div");
+        me.classList.add("me");
+        me.textContent = "ME";
+
+        const meWidth = 50; // Adjust this value according to the width set in AnimateMes.css
+        const meHeight = 24; // Adjust this value according to the height set in AnimateMes.css
+        const xPos =
+          Math.random() * (animationDiv.current.clientWidth - meWidth);
+        const yPos = Math.random() * (height - meHeight);
+        me.style.left = `${xPos}px`;
+        me.style.top = `${yPos}px`;
+
+        const delay = Math.floor(Math.random() * 2000);
+        me.style.animationDelay = `${delay}ms`;
+
+        animationDiv.current.appendChild(me);
+      }
+    }
+  }, [height, windowWidth]);
+
+  return <div className="animation" ref={animationDiv}></div>;
 }
--- frontend/src/components/HeroBanner/index.js
+++ frontend/src/components/HeroBanner/index.js
@@ -1,15 +1,41 @@
+import React, { useRef, useState, useEffect } from "react";
 import "./HeroBanner.css";
 import BannerButton from "../BannerButton";
+import AnimateMes from "./animees";
 
 const HeroBanner = () => {
+  const heroBannerRef = useRef(null);
+
+  const [height, setHeight] = useState(0);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    if (heroBannerRef.current) {
+      setHeight(heroBannerRef.current.clientHeight);
+    }
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="hero-banner">
+    <div className="hero-banner" ref={heroBannerRef}>
       <div className="text">
         <h1>Fly, you fools!</h1>
         <h2>There is nothing like looking if you want to find something.</h2>
         <BannerButton></BannerButton>
       </div>
-      <div className="animation"></div>
+      <div className="animation">
+        <AnimateMes height={height} windowWidth={windowWidth} />
+      </div>
     </div>
   );
 };
