05989db0da68fb67284cb9f836a15472f08b0be5
lorenzo <lorenzotijerina@gmail.com>
2023-05-03 14:41:18 -0400

    refactor root reducer to only use logger in dev

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -12,15 +12,16 @@ const rootReducer = combineReducers({
   avatars: avatarsReducer,
 });
 
-let enhancer;
-
-// if (process.env.NODE_ENV === "production") {
-//   enhancer = applyMiddleware(thunk);
-// } else {
-const logger = require("redux-logger").default;
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-// }
+
+const middleware = [thunk];
+
+if (process.env.NODE_ENV !== "production") {
+  const logger = require("redux-logger").default;
+  middleware.push(logger);
+}
+
+const enhancer = composeEnhancers(applyMiddleware(...middleware));
 
 const configureStore = (preloadedState) => {
   return createStore(rootReducer, preloadedState, enhancer);
